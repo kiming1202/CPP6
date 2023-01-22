6bf2e568ed157ea57330a650f02fc1388aa403d7
kiming1202 <4428088@qq.com>
2023-01-23 04:00:04 +0800

    测试

---
+++ CH01/test2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+    int a = 0 , b= 3, c = 4;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("a = %d\nb = %d\nc = %d\n",a,a*b,a*b*c);
+    return 0;
+}
